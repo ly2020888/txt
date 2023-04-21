a3b598533a778ebf914078fbc24c3b0e5457d8ea
刘毅 <9458440+lin-zhongzhi@user.noreply.gitee.com>
2023-04-21 21:35:21 +0800

    添加了todolist

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -4,10 +4,15 @@ use tokio::net::TcpStream;
 use txt::frame::TxtFrame;
 use txt::Connection;
 
+// To-DO: 使用环境变量或配置文件取代硬编码
+const IP: &str = "127.0.0.1:9000";
+
+// To-DO: 实现命令解析，发起上传，下载、获取索引等功能
+// 完善命令行工具的提示信息。
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // Connect to a peer
-    let stream = TcpStream::connect("127.0.0.1:9000").await?;
+    let stream = TcpStream::connect(IP).await?;
 
     // Write some data.
     let mut con = Connection::new(stream);
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,16 +1,16 @@
-use txt::Connection;
+use txt::{frame::TxtFrame, Connection};
 
 use std::io;
 use tokio::net::{TcpListener, TcpStream};
 
+// To-DO: 使用环境变量或配置文件取代硬编码
 const IP: &str = "127.0.0.1:9000";
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let listener = TcpListener::bind(IP).await?;
-
+    println!("准备开始接受请求");
     loop {
-        println!("开始监听");
         let (stream, _) = listener.accept().await.unwrap();
         process(stream).await;
     }
@@ -19,8 +19,26 @@ async fn main() -> io::Result<()> {
 async fn process(stream: TcpStream) {
     let _ = tokio::spawn(async move {
         let mut con = Connection::new(stream);
-        let one_frame = con.read_frame().await;
-        println!("收到:{:?}", one_frame);
-    })
-    .await;
+        loop {
+            let res = con.read_frame().await;
+            match res {
+                Ok(op) => {
+                    if let Some(frame) = op {
+                        // 处理数据帧
+                        action(&mut con, frame).await;
+                        continue;
+                    }
+                    return;
+                }
+                Err(_) => {
+                    return;
+                }
+            }
+        }
+    });
+}
+
+// To-DO: 处理一个数据帧，完成上传，下载，获取索引的功能
+async fn action(_con: &mut Connection, _frame: TxtFrame) {
+    unimplemented!();
 }
